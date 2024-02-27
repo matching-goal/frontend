7e2658c2a5d5c0cf662a499aa196aae6dc5e5aef
pjc0179 <pjc0179@gmail.com>
2024-02-27 19:45:19 +0900

    feat: 임시 매칭 sort 데이터 생성

---
+++ src/constants/sort.ts
@@ -0,0 +1,5 @@
+export const matchingSortList = [
+  { name: '최신 순', key: 'created_date', type: 'first' },
+  { name: '오래된 순', key: 'created_date', type: 'last' },
+  { name: '조회수 순', key: 'count', type: 'first' },
+];
