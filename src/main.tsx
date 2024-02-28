2024250d6c6ba8c9d16c887c6748411b63c7f49e
pjc0179 <pjc0179@gmail.com>
2024-02-28 13:38:56 +0900

    feat: react-calendar css 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import 'react-calendar/dist/Calendar.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
