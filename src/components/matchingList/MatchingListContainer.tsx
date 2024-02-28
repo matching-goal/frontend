b6195c4e0152e0e5684717f136fbcfb3ee2ee8fb
pjc0179 <pjc0179@gmail.com>
2024-02-28 21:55:19 +0900

    feat: ErrorBoundary 추가

--- src/components/matchingList/MatchingListContainer.tsx
+++ src/components/matchingList/MatchingListContainer.tsx
@@ -1,14 +1,18 @@
-import { Suspense } from 'react';
-import MatchingList from './MatchingList';
+import React, { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+
+const MatchingList = React.lazy(() => import('./MatchingList'));
 
 const MatchingListContainer = () => {
   return (
     <main>
       <section className=" max-w-screen-md mx-auto">
         <h1 className="text-center font-bold text-[30px]">매칭 목록</h1>
-        <Suspense fallback={<div>로딩중~</div>}>
-          <MatchingList></MatchingList>
-        </Suspense>
+        <ErrorBoundary fallback={<h1>데이터 패칭 중 에러 발생</h1>}>
+          <Suspense fallback={<div>로딩중~</div>}>
+            <MatchingList></MatchingList>
+          </Suspense>
+        </ErrorBoundary>
       </section>
     </main>
   );
