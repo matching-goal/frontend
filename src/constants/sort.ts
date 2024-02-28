3125f3d8cc78f1af2f499e202f29bdf70520a2fc
pjc0179 <pjc0179@gmail.com>
2024-02-28 16:19:30 +0900

    feat: 데이터 추가

--- src/constants/sort.ts
+++ src/constants/sort.ts
@@ -1,4 +1,5 @@
 export const matchingSortList = [
+  { name: '정렬 선택', type: 'default' },
   { name: '최신 순', key: 'created_date', type: 'first' },
   { name: '오래된 순', key: 'created_date', type: 'last' },
   { name: '조회수 순', key: 'count', type: 'first' },
