1b9d887179c386f1cf02cbe03d9a44ad57fa294f
pjc0179 <pjc0179@gmail.com>
2024-02-29 15:56:22 +0900

    feat: matching create mutation 구현

---
+++ src/mutations/useMatchingCreate.ts
@@ -0,0 +1,20 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import API from '../api/api';
+import { useNavigate } from 'react-router-dom';
+import { CreateMatching } from '../interface/matching';
+
+const useMatchingCreate = () => {
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (data: CreateMatching) => API.post(`/api/matching`, data),
+
+    onSuccess: (data) => {
+      queryClient.removeQueries({ queryKey: ['PreviewMatchingList'] });
+      queryClient.setQueryData(['matching', data.data.id], data.data);
+      navigate(`/matching/${data.data.id}`);
+    },
+  });
+};
+
+export default useMatchingCreate;
