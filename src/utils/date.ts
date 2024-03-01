a3b40788c5ede1b7b382333facd26de9440259ed
pjc0179 <pjc0179@gmail.com>
2024-03-01 16:23:04 +0900

    feat: 숫자 한자리의 경우 앞에 0을 붙이는 기능 추가

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,2 +1,5 @@
 export const formatDate = (date: Date) =>
-  `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+  `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date
+    .getDate()
+    .toString()
+    .padStart(2, '0')}`;
