fcfa7ce443bfc6e145eaefe688b399389adea839
pjc0179 <pjc0179@gmail.com>
2024-03-01 16:22:07 +0900

    feat: 타입 변경에따른 타입수정

--- src/mocks/handlers/matching.ts
+++ src/mocks/handlers/matching.ts
@@ -1,5 +1,6 @@
 import { HttpResponse, http, delay } from 'msw';
 import { CreateMatching, ViewMatching } from '../../interface/matching';
+import { formatDate } from '../../utils/date';
 let matchingList: ViewMatching[] = [
   {
     id: '1',
@@ -20,8 +21,9 @@ let matchingList: ViewMatching[] = [
       'http://www.dreamparkcf.com/images/sub/res01_10.jpg',
     ],
     nickName: '김땡땡',
-    matchDate: '2024-05-12',
+    matchingDate: '2024-05-12',
     content: '매너있는 경기 추구합니다 1:1채팅 부탁드려요',
+    matchingTime: '13:15',
   },
   {
     id: '2',
@@ -42,8 +44,9 @@ let matchingList: ViewMatching[] = [
       'http://www.dreamparkcf.com/images/sub/res01_10.jpg',
     ],
     nickName: '김땡땡',
-    matchDate: '2024-05-12',
+    matchingDate: '2024-05-12',
     content: '매너있는 경기 추구합니다 1:1채팅 부탁드려요',
+    matchingTime: '13:15',
   },
   {
     id: '3',
@@ -64,8 +67,9 @@ let matchingList: ViewMatching[] = [
       'http://www.dreamparkcf.com/images/sub/res01_10.jpg',
     ],
     nickName: '김땡땡',
-    matchDate: '2024-05-12',
+    matchingDate: '2024-05-12',
     content: '매너있는 경기 추구합니다 1:1채팅 부탁드려요',
+    matchingTime: '13:15',
   },
   {
     id: '4',
@@ -86,206 +90,9 @@ let matchingList: ViewMatching[] = [
       'http://www.dreamparkcf.com/images/sub/res01_10.jpg',
     ],
     nickName: '김땡땡',
-    matchDate: '2024-05-12',
+    matchingDate: '2024-05-12',
     content: '매너있는 경기 추구합니다 1:1채팅 부탁드려요',
-  },
-  {
-    id: '5',
-    memberId: '127',
-    teamImg:
-      'https://upload.wikimedia.org/wikipedia/ko/b/b1/FC_%EB%B0%94%EB%A5%B4%EC%85%80%EB%A1%9C%EB%82%98_%EB%A1%9C%EA%B3%A0.svg',
-    title: '서울시 서초구에서 경기하실분 구해요~',
-    createdDate: '2024-04-27',
-    modified_date: '2024-04-28',
-    count: 17,
-    status: '모집중',
-    requestCount: 0,
-    stadium: '서울시 서초구 xx로 땡땡경기장',
-    img: [
-      'http://www.dreamparkcf.com/images/sub/res01_05.jpg',
-      'http://www.dreamparkcf.com/images/sub/res01_06.jpg',
-      'http://www.dreamparkcf.com/images/sub/res01_07.jpg',
-      'http://www.dreamparkcf.com/images/sub/res01_10.jpg',
-    ],
-    nickName: '김땡땡',
-    matchDate: '2024-05-12',
-    content: '매너있는 경기 추구합니다 1:1채팅 부탁드려요',
-  },
-  {
-    id: '6',
-    memberId: '128',
-    teamImg:
-      'https://upload.wikimedia.org/wikipedia/ko/b/b1/FC_%EB%B0%94%EB%A5%B4%EC%85%80%EB%A1%9C%EB%82%98_%EB%A1%9C%EA%B3%A0.svg',
-    title: '경기도 안양시에서 경기하실분 구해요~',
-    createdDate: '2024-04-27',
-    modified_date: '2024-04-28',
-    count: 16,
-    status: '모집중',
-    requestCount: 1,
-    stadium: '경기도 안양시 xx로 축구장',
-    img: [
-      'http://www.dreamparkcf.com/images/sub/res01_05.jpg',
-      'http://www.dreamparkcf.com/images/sub/res01_06.jpg',
-      'http://www.dreamparkcf.com/images/sub/res01_07.jpg',
-      'http://www.dreamparkcf.com/images/sub/res01_10.jpg',
-    ],
-    nickName: '김땡땡',
-    matchDate: '2024-05-12',
-    content: '매너있는 경기 추구합니다 1:1채팅 부탁드려요',
-  },
-  {
-    id: '7',
-    memberId: '129',
-    teamImg:
-      'https://upload.wikimedia.org/wikipedia/ko/b/b1/FC_%EB%B0%94%EB%A5%B4%EC%85%80%EB%A1%9C%EB%82%98_%EB%A1%9C%EA%B3%A0.svg',
-    title: '인천광역시 계양구에서 경기하실분 구해요~',
-    createdDate: '2024-04-27',
-    modified_date: '2024-04-28',
-    count: 14,
-    status: '모집중',
-    requestCount: 3,
-    stadium: '인천광역시 계양구 xx로 땡땡경기장',
-    img: [
-      'http://www.dreamparkcf.com/images/sub/res01_05.jpg',
-      'http://www.dreamparkcf.com/images/sub/res01_06.jpg',
-      'http://www.dreamparkcf.com/images/sub/res01_07.jpg',
-      'http://www.dreamparkcf.com/images/sub/res01_10.jpg',
-    ],
-    nickName: '김땡땡',
-    matchDate: '2024-05-12',
-    content: '매너있는 경기 추구합니다 1:1채팅 부탁드려요',
-  },
-  {
-    id: '8',
-    memberId: '130',
-    teamImg:
-      'https://upload.wikimedia.org/wikipedia/ko/b/b1/FC_%EB%B0%94%EB%A5%B4%EC%85%80%EB%A1%9C%EB%82%98_%EB%A1%9C%EA%B3%A0.svg',
-    title: '경기도 고양시에서 경기하실분 구해요~',
-    createdDate: '2024-04-27',
-    modified_date: '2024-04-28',
-    count: 19,
-    status: '모집중',
-    requestCount: 2,
-    stadium: '경기도 고양시 xx로 축구장',
-    img: [
-      'http://www.dreamparkcf.com/images/sub/res01_05.jpg',
-      'http://www.dreamparkcf.com/images/sub/res01_06.jpg',
-      'http://www.dreamparkcf.com/images/sub/res01_07.jpg',
-      'http://www.dreamparkcf.com/images/sub/res01_10.jpg',
-    ],
-    nickName: '김땡땡',
-    matchDate: '2024-05-12',
-    content: '매너있는 경기 추구합니다 1:1채팅 부탁드려요',
-  },
-  {
-    id: '9',
-    memberId: '131',
-    teamImg:
-      'https://upload.wikimedia.org/wikipedia/ko/b/b1/FC_%EB%B0%94%EB%A5%B4%EC%85%80%EB%A1%9C%EB%82%98_%EB%A1%9C%EA%B3%A0.svg',
-    title: '서울시 송파구에서 경기하실분 구해요~',
-    createdDate: '2024-04-27',
-    modified_date: '2024-04-28',
-    count: 21,
-    status: '모집중',
-    requestCount: 0,
-    stadium: '서울시 송파구 xx로 땡땡경기장',
-    img: [
-      'http://www.dreamparkcf.com/images/sub/res01_05.jpg',
-      'http://www.dreamparkcf.com/images/sub/res01_06.jpg',
-      'http://www.dreamparkcf.com/images/sub/res01_07.jpg',
-      'http://www.dreamparkcf.com/images/sub/res01_10.jpg',
-    ],
-    nickName: '김땡땡',
-    matchDate: '2024-05-12',
-    content: '매너있는 경기 추구합니다 1:1채팅 부탁드려요',
-  },
-  {
-    id: '10',
-    memberId: '132',
-    teamImg:
-      'https://upload.wikimedia.org/wikipedia/ko/b/b1/FC_%EB%B0%94%EB%A5%B4%EC%85%80%EB%A1%9C%EB%82%98_%EB%A1%9C%EA%B3%A0.svg',
-    title: '경기도 의정부시에서 경기하실분 구해요~',
-    createdDate: '2024-04-27',
-    modified_date: '2024-04-28',
-    count: 23,
-    status: '모집중',
-    requestCount: 1,
-    stadium: '경기도 의정부시 xx로 축구장',
-    img: [
-      'http://www.dreamparkcf.com/images/sub/res01_05.jpg',
-      'http://www.dreamparkcf.com/images/sub/res01_06.jpg',
-      'http://www.dreamparkcf.com/images/sub/res01_07.jpg',
-      'http://www.dreamparkcf.com/images/sub/res01_10.jpg',
-    ],
-    nickName: '김땡땡',
-    matchDate: '2024-05-12',
-    content: '매너있는 경기 추구합니다 1:1채팅 부탁드려요',
-  },
-  {
-    id: '11',
-    memberId: '133',
-    teamImg:
-      'https://upload.wikimedia.org/wikipedia/ko/b/b1/FC_%EB%B0%94%EB%A5%B4%EC%85%80%EB%A1%9C%EB%82%98_%EB%A1%9C%EA%B3%A0.svg',
-    title: '경기도 부천시에서 경기하실분 구해요~',
-    createdDate: '2024-04-27',
-    modified_date: '2024-04-28',
-    count: 18,
-    status: '모집중',
-    requestCount: 2,
-    stadium: '경기도 부천시 xx로 땡땡경기장',
-    img: [
-      'http://www.dreamparkcf.com/images/sub/res01_05.jpg',
-      'http://www.dreamparkcf.com/images/sub/res01_06.jpg',
-      'http://www.dreamparkcf.com/images/sub/res01_07.jpg',
-      'http://www.dreamparkcf.com/images/sub/res01_10.jpg',
-    ],
-    nickName: '김땡땡',
-    matchDate: '2024-05-12',
-    content: '매너있는 경기 추구합니다 1:1채팅 부탁드려요',
-  },
-  {
-    id: '12',
-    memberId: '134',
-    teamImg:
-      'https://upload.wikimedia.org/wikipedia/ko/b/b1/FC_%EB%B0%94%EB%A5%B4%EC%85%80%EB%A1%9C%EB%82%98_%EB%A1%9C%EA%B3%A0.svg',
-    title: '경기도 광명시에서 경기하실분 구해요~',
-    createdDate: '2024-04-27',
-    modified_date: '2024-04-28',
-    count: 16,
-    status: '모집중',
-    requestCount: 0,
-    stadium: '경기도 광명시 xx로 축구장',
-    img: [
-      'http://www.dreamparkcf.com/images/sub/res01_05.jpg',
-      'http://www.dreamparkcf.com/images/sub/res01_06.jpg',
-      'http://www.dreamparkcf.com/images/sub/res01_07.jpg',
-      'http://www.dreamparkcf.com/images/sub/res01_10.jpg',
-    ],
-    nickName: '김땡땡',
-    matchDate: '2024-05-12',
-    content: '매너있는 경기 추구합니다 1:1채팅 부탁드려요',
-  },
-  {
-    id: '13',
-    memberId: '135',
-    teamImg:
-      'https://upload.wikimedia.org/wikipedia/ko/b/b1/FC_%EB%B0%94%EB%A5%B4%EC%85%80%EB%A1%9C%EB%82%98_%EB%A1%9C%EA%B3%A0.svg',
-    title: '경기도 구리시에서 경기하실분 구해요~',
-    createdDate: '2024-04-27',
-    modified_date: '2024-04-28',
-    count: 14,
-    status: '모집중',
-    requestCount: 1,
-    stadium: '경기도 구리시 xx로 땡땡경기장',
-    img: [
-      'http://www.dreamparkcf.com/images/sub/res01_05.jpg',
-      'http://www.dreamparkcf.com/images/sub/res01_06.jpg',
-      'http://www.dreamparkcf.com/images/sub/res01_07.jpg',
-      'http://www.dreamparkcf.com/images/sub/res01_10.jpg',
-    ],
-    nickName: '김땡땡',
-    matchDate: '2024-05-12',
-    content: '매너있는 경기 추구합니다 1:1채팅 부탁드려요',
+    matchingTime: '13:15',
   },
 ];
 const getMatchingList = http.get('/api/matching/list', async () => {
@@ -316,15 +123,11 @@ const postMatching = http.post('/api/matching', async ({ request }) => {
     status: '모집중',
     count: 1,
     requestCount: 0,
-    memberId: body.memberId,
-    title: body.title,
-    stadium: body.stadium,
-    content: body.content,
-    createdDate: body.matchDate,
-    img: body.img as string[],
-    matchDate: body.matchDate,
+    ...body,
+    createdDate: formatDate(new Date()),
   };
   matchingList = [...matchingList, newMatching];
+  await delay(1000);
   return HttpResponse.json(newMatching);
 });
 
@@ -334,9 +137,24 @@ const deleteMatching = http.delete('/api/matching/:id', ({ params }) => {
   return HttpResponse.json('삭제 성공');
 });
 
+const patchMatching = http.patch('/api/matching/:id', async ({ params, request }) => {
+  const id = params.id as string;
+  const body = (await request.json()) as CreateMatching;
+  let newMatching;
+  matchingList = matchingList.map((matching) => {
+    if (matching.id !== id) {
+      return matching;
+    }
+    newMatching = { ...matching, ...body };
+    return newMatching;
+  });
+  return HttpResponse.json(newMatching);
+});
+
 export const matchingHandlers = [
   getMatchingList,
   getMatching,
   postMatching,
   deleteMatching,
+  patchMatching,
 ];
