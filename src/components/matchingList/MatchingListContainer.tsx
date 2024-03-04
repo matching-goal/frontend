d792ce58d0f12faa5279e27a24b53ba964eb719b
pjc0179 <pjc0179@gmail.com>
2024-03-04 16:09:57 +0900

    feat:"날짜 기준 필터링 query 전달기능 추가"

--- src/components/matchingList/MatchingListContainer.tsx
+++ src/components/matchingList/MatchingListContainer.tsx
@@ -1,16 +1,57 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useState } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
+import DateSelectorBtn from '../button/DateSelectorBtn';
+import TimeSelectorBtn from '../button/TimeSelectorBtn';
+import { dateFormatRegex } from '../../utils/regex';
 
 const MatchingList = React.lazy(() => import('./MatchingList'));
 
 const MatchingListContainer = () => {
+  const [date, setDate] = useState('날짜 선택');
+  const [hour, setHour] = useState('시간 선택');
+  const [minute, setMinute] = useState('');
+  const [query, setQuery] = useState('');
   return (
     <main>
       <section className=" max-w-screen-md mx-auto">
         <h1 className="text-center font-bold text-[30px]">매칭 목록</h1>
+        <div className="flex justify-between">
+          <div className="flex">
+            <div className="mr-5">
+              <DateSelectorBtn
+                date={date}
+                onChange={(e) => {
+                  setDate(e);
+                }}
+              ></DateSelectorBtn>
+            </div>
+            <div>
+              <TimeSelectorBtn
+                hour={hour}
+                minute={minute}
+                onChange={(hour, minute) => {
+                  setHour(hour);
+                  setMinute(minute);
+                }}
+              ></TimeSelectorBtn>
+            </div>
+          </div>
+          <div>
+            <button
+              className="btn border border-gray-300"
+              onClick={() => {
+                let query = '';
+                dateFormatRegex.test(date) && (query += `date=${date}`);
+                setQuery(query);
+              }}
+            >
+              필터 적용
+            </button>
+          </div>
+        </div>
         <ErrorBoundary fallback={<h1>데이터 패칭 중 에러 발생</h1>}>
           <Suspense fallback={<div>로딩중~</div>}>
-            <MatchingList></MatchingList>
+            <MatchingList query={query}></MatchingList>
           </Suspense>
         </ErrorBoundary>
       </section>
