c683ad49d2f827f6a6d378a28d34a8bcb698ea9a
pjc0179 <pjc0179@gmail.com>
2024-03-06 04:20:29 +0900

    chore:"data.data로 반환하던 API가 변경됨에 따라 코드 수정"

--- src/queries/useGetMatching.ts
+++ src/queries/useGetMatching.ts
@@ -1,10 +1,12 @@
+'use client';
+
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { ViewMatching } from '../interface/matching';
 import API from '../api/api';
 
 const useGetMatching = (id: string) => {
   return useSuspenseQuery<ViewMatching>({
-    queryFn: async () => await API.get(`/api/matching/${id}`),
+    queryFn: async () => (await API.get(`/api/matching/${id}`)).data,
     queryKey: ['matching', id],
   });
 };
