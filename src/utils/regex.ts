91d901950c56ef79c033a128f1d158d2e4df4ada
pjc0179 <pjc0179@gmail.com>
2024-03-05 13:13:46 +0900

    feat: 이메일 형식, 패스워드형식 검사하는 정규식 추가

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -1,1 +1,5 @@
 export const dateFormatRegex = /^\d{4}-\d{2}-\d{2}$/;
+
+export const passwordRegex = /^(?=.*[0-9])(?=.*[a-zA-Z])[a-zA-Z0-9!@#$%^&*()]*$/;
+
+export const emailRegex = /^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/;
