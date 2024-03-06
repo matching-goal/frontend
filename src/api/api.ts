643439e12b8587cfa8f8acda86f6fdebf261f3e5
pjc0179 <pjc0179@gmail.com>
2024-03-06 20:30:35 +0900

    feat:"api header 추가"

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,25 +1,57 @@
 import axios from 'axios';
 import BASE_API_URL from '../constants/url';
 
+import { getSession } from 'next-auth/react';
+
 const API = {
   get: async (url: string) => {
-    const res = await axios.get(`${BASE_API_URL + url}`);
+    const session = await getSession();
+    const token = session?.accessToken;
+    const res = await axios.get(`${BASE_API_URL + url}`, {
+      headers: {
+        Authorization: `Bearer ${token?.email}`,
+      },
+    });
     return res;
   },
   post: async <T>(url: string, body: T) => {
-    const res = await axios.post(`${BASE_API_URL + url}`, body);
+    const session = await getSession();
+    const token = session?.accessToken;
+    const res = await axios.post(`${BASE_API_URL + url}`, body, {
+      headers: {
+        Authorization: `Bearer ${token?.email}`,
+      },
+    });
     return res;
   },
   delete: async (url: string) => {
-    const res = await axios.delete(`${BASE_API_URL + url}`);
+    const session = await getSession();
+    const token = session?.accessToken;
+    const res = await axios.delete(`${BASE_API_URL + url}`, {
+      headers: {
+        Authorization: `Bearer ${token?.email}`,
+      },
+    });
     return res;
   },
   put: async <T>(url: string, body: T) => {
-    const res = await axios.put(`${BASE_API_URL + url}`, body);
+    const session = await getSession();
+    const token = session?.accessToken;
+    const res = await axios.put(`${BASE_API_URL + url}`, body, {
+      headers: {
+        Authorization: `Bearer ${token?.email}`,
+      },
+    });
     return res;
   },
   patch: async <T>(url: string, body: T) => {
-    const res = await axios.patch(`${BASE_API_URL + url}`, body);
+    const session = await getSession();
+    const token = session?.accessToken;
+    const res = await axios.patch(`${BASE_API_URL + url}`, body, {
+      headers: {
+        Authorization: `Bearer ${token?.email}`,
+      },
+    });
     return res;
   },
 };
