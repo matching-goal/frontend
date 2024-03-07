07f91e0c8414fb59aad159f90f8ddcbb95d4bc25
pjc0179 <pjc0179@gmail.com>
2024-03-07 16:03:59 +0900

    feat:"calendar 에 경기일정  표시하게 추가"

--- src/components/team/TeamCalendar.tsx
+++ src/components/team/TeamCalendar.tsx
@@ -1,12 +1,26 @@
+'use client';
+import useGetCalendarData from '@/queries/useGetCalendarData';
+import { formatDate } from '@/utils/date';
+import { useParams } from 'next/navigation';
 import Calendar from 'react-calendar';
 
 const TeamCalendar = () => {
+  const params = useParams();
+  const id = params.id as string;
+  const { data: calendarData } = useGetCalendarData(id);
+
   return (
     <div className="max-w-screen-md mx-auto">
       <Calendar
         tileClassName={() => 'flex flex-col'}
         tileContent={({ activeStartDate, date, view }) => {
-          return <div className="">asdasdf</div>;
+          for (let i = 0; i < calendarData.length; i++) {
+            if (formatDate(date) === calendarData[i].date) {
+              return (
+                <div className=" text-red-500">{`${calendarData[i].opponent}팀 과의 경기`}</div>
+              );
+            }
+          }
         }}
       ></Calendar>
     </div>
