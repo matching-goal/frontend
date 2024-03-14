930cef317fcbe4c14b381a402a60fa6dd345e415
pjc0179 <pjc0179@gmail.com>
2024-03-14 15:36:49 +0900

    style:"팀프로필 스타일변경"

--- src/components/team/teamProfile/TeamProfile.tsx
+++ src/components/team/teamProfile/TeamProfile.tsx
@@ -1,5 +1,6 @@
 'use client';
 import useGetTeam from '@/queries/useGetTeam';
+import { getImageOrDefault } from '@/utils/image';
 import { useParams } from 'next/navigation';
 
 const TeamProfile = () => {
@@ -8,31 +9,36 @@ const TeamProfile = () => {
   const { data: team } = useGetTeam(id);
 
   return (
-    <div className="max-w-screen-md mx-auto  w-full grid grid-cols-4  h-[150px]">
-      <div className="col-span-1 flex items-center">
-        <figure className="w-[100px] h-[100px]">
-          <img
-            src={team.teamImg === '' ? '/defaultProfileImg.jpg' : team.teamImg}
-            alt="팀이미지"
-            className="w-full h-full"
-          />
-        </figure>
+    <div className="hero-content text-center max-w-screen-md mx-auto">
+      <div className="">
+        <h1 className="text-4xl font-bold mb-5">{team.nickname}</h1>
+        <div className="avatar">
+          <div className=" w-52 rounded-full ">
+            <img
+              src={getImageOrDefault(team.teamImg)}
+              className="object-contain w-5 h-5"
+            />
+            <div>{team.introduction}</div>
+          </div>
+        </div>
+        <div className="stats shadow w-full">
+          <div className="stat">
+            <div className="stat-figure text-primary"></div>
+            <div className="stat-title">경기전적</div>
+            <div className="stat-value text-primary">2승1무1패</div>
+          </div>
+
+          <div className="stat">
+            <div className="stat-title">승률</div>
+            <div className="stat-value text-secondary">50%</div>
+          </div>
+
+          <div className="stat">
+            <div className="stat-title">평점</div>
+            <div className="stat-value ">7.8</div>
+          </div>
+        </div>
       </div>
-      <div className="flex flex-col justify-between h-full col-span-2">
-        <div>
-          <p>{team.nickname}</p>
-        </div>
-        <div>
-          <p>승률: 승률표시</p>
-        </div>
-        <div>
-          <p>평점: 평점표시</p>
-        </div>
-        <div>
-          <p>경기 통계: 통계표시</p>
-        </div>
-      </div>
-      <div className="col-span-1 flex items-center">{team.introduction}</div>
     </div>
   );
 };
