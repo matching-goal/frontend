cda83415842f2c6da196134d45bc9f2239f584d6
pjc0179 <pjc0179@gmail.com>
2024-03-16 12:45:35 +0900

    feat:"에러처리를 team page에서 하게 변경"

--- src/app/team/[id]/page.tsx
+++ src/app/team/[id]/page.tsx
@@ -1,15 +1,20 @@
 import TeamNav from '@/components/team/TeamNav';
 import TeamProfileContainer from '@/components/team/teamProfile/TeamProfileContainer';
 
+import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+
 const page = () => {
   return (
-    <article>
-      <section>
-        <TeamProfileContainer></TeamProfileContainer>
-      </section>
-      <section className="mt-20">
-        <TeamNav></TeamNav>
-      </section>
+    <article className="min-h-screen">
+      <ErrorBoundary fallback={<div>유저 정보 에러</div>}>
+        <section>
+          <TeamProfileContainer />
+        </section>
+        <section className="mt-20">
+          <TeamNav></TeamNav>
+        </section>
+      </ErrorBoundary>
     </article>
   );
 };
