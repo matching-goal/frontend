9f221ba9895f773db88c42e9b097e2b279a51e83
pjc0179 <pjc0179@gmail.com>
2024-03-20 19:53:00 +0900

    feat:" 매칭 리스트 필터 및 정렬 추가"

--- src/components/matchingList/MatchingListContainer.tsx
+++ src/components/matchingList/MatchingListContainer.tsx
@@ -2,54 +2,29 @@
 
 import React, { Suspense, useState } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
-import DateSelectorBtn from '../button/DateSelectorBtn';
-import TimeSelectorBtn from '../button/TimeSelectorBtn';
-import { dateFormatRegex } from '../../utils/regex';
 import MatchingList from './MatchingList';
+import MatchingAndTeamListFilter from '../filter/MatchingAndTeamListFilter';
+import SelectSort from '../util/SelectSort';
 
 const MatchingListContainer = () => {
-  const [date, setDate] = useState('날짜 선택');
-  const [hour, setHour] = useState('시간 선택');
-  const [minute, setMinute] = useState('');
-  const [query, setQuery] = useState('');
+  const [filterQuery, setFilterQuery] = useState('');
+  const [sortQuery, setSortQuery] = useState('');
+  const query = filterQuery + sortQuery;
+  console.log(query);
   return (
     <main>
       <section className=" max-w-screen-md mx-auto">
         <h1 className="text-center font-bold text-[30px]">매칭 목록</h1>
         <div className="flex justify-between">
-          <div className="flex">
-            <div className="mr-5">
-              <DateSelectorBtn
-                date={date}
-                onChange={(e) => {
-                  setDate(e);
-                }}></DateSelectorBtn>
-            </div>
-            <div>
-              <TimeSelectorBtn
-                hour={hour}
-                minute={minute}
-                onChange={(hour, minute) => {
-                  setHour(hour);
-                  setMinute(minute);
-                }}></TimeSelectorBtn>
-            </div>
-          </div>
-          <div>
-            <button
-              className="btn border border-gray-300"
-              onClick={() => {
-                let query = '';
-                dateFormatRegex.test(date) && (query += `date=${date}`);
-                setQuery(query);
-              }}>
-              필터 적용
-            </button>
-          </div>
+          <MatchingAndTeamListFilter
+            setQuery={setFilterQuery}
+            query={filterQuery}
+          />
+          <SelectSort setQuery={setSortQuery} />
         </div>
         <ErrorBoundary fallback={<h1>데이터 패칭 중 에러 발생</h1>}>
           <Suspense fallback={<div>로딩중~</div>}>
-            <MatchingList query={query}></MatchingList>
+            <MatchingList query={query} />
           </Suspense>
         </ErrorBoundary>
       </section>
